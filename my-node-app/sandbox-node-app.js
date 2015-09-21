86f7d9e593e77adb64468315ff42b875c8e225c1
Matthew Pengelly <pengelly.mat@gmail.com>
2015-09-20 20:52:40 -0400

    implemented handlebars basic templating

--- my-node-app/sandbox-node-app.js
+++ my-node-app/sandbox-node-app.js
@@ -4,27 +4,29 @@ var app = express();
 app.set('title', "Test site");
 app.set('port', process.env.PORT || 3000);
 
+app.use(express.static(__dirname + '/public'));
+
+var handlebars = require('express3-handlebars').create({defaultLayout:'main'});
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
 app.get('/', function (req, res) {
-  res.type('text/plain');
-  res.send('This is the home page.');
+  res.render('home');
 });
 
 app.get('/about', function (req, res) {
-  res.type('text/plain');
-  res.send("Page about me.");
+  res.render('about');
 });
 
 app.use(function (req, res) {
-  res.type('text/plain');
   res.status('404');
-  res.send('404 - Not Found.');
+  res.render('404');
 });
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
-  res.type('text/plain');
   res.status('500');
-  res.send('500 - Server error.');
+  res.render('500');
 });
 
 
