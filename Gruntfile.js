783c360233a52878a403065911da9a13660a8676
Matthew Pengelly <pengelly.mat@gmail.com>
2015-10-11 18:27:09 -0400

    modified gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,4 +9,18 @@ module.exports = function(grunt) {
 	});
 
 	//configure plugins
-}
+	grunt.initConfig({
+		cafemocha: {
+			all: { src: 'qa/test-*.js', options : {ul: 'tdd'} }
+		},
+		jshint: {
+			app: ['meadowlark.js', 'public/js/**/*.js', 'lib/**/*.js'],
+			qa: ['Gruntfile.js', 'public/qa/**/*.js', 'qa/**/*.js'],
+			exec: { linkchecker: { cmd: 'linkchecker http://localhost/3000'}}
+	},
+	});
+
+	//register tasks
+	//grunt.registerTask('default', ['cafemocha', 'jshint', 'exec']);
+	grunt.registerTask('default', ['cafemocha', 'jshint']);
+};
